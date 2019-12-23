3f19064816c47572f20b8f991a641d4b98558359
tsehsuan1102 <sam9883043@gmail.com>
2019-12-24 02:29:18 +0800

    Add Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed = 1.5f;
+	private float rotatespeed = 50.0f;
+
+	public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+	public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+
+	private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+	private string m_TurnAxisName;              // The name of the input axis for turning.
+	private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+	private float m_MovementInputValue;         // The current value of the movement input.
+	private float m_TurnInputValue;             // The current value of the turn input.
+	private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+
+	public Transform m_FireTransform;
+	public Transform m_Camera;
+
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+
+	private void OnEnable()
+	{
+		// When the tank is turned on, make sure it's not kinematic.
+		m_Rigidbody.isKinematic = false;
+
+		// Also reset the input values.
+		m_MovementInputValue = 0f;
+		m_TurnInputValue = 0f;
+	}
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		m_MovementAxisName = "Vertical";
+		m_TurnAxisName = "Horizontal";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+		m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+		/*
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(Vector3.down * Time.deltaTime * rotatespeed);
+			//transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(Vector3.up * Time.deltaTime * rotatespeed);
+			//transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.back * speed * Time.deltaTime;
+		}*/
+    }
+
+	private void FixedUpdate()
+	{
+		// Adjust the rigidbodies position and orientation in FixedUpdate.
+		Move();
+		Turn();
+	}
+
+	private void Move()
+	{
+		// Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+		// Apply this movement to the rigidbody's position.
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+	}
+	private void Turn()
+	{
+		// Determine the number of degrees to be turned based on the input, speed and time between frames.
+		float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+		// Make this into a rotation in the y axis.
+		Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+		// Apply this rotation to the rigidbody's rotation.
+		m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+	}
+
+}
